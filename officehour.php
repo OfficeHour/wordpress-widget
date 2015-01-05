8efabc3abe34ba16418c0731dd70d7094601b231
wawagit <guillaume.warckol@gmail.com>
2015-01-05 18:01:49 +0100

    formulaire d'edition du widget

--- officehour.php
+++ officehour.php
@@ -26,6 +26,19 @@ class OfficeHour_Widget extends WP_Widget
         	echo $before_title.$d['title'].$after_title;
 	}
 
+        if(isset($d['slug']) && !empty($d['slug'])) {
+        ?>
+        <a id='officehour_widget' data-slug='<?php echo $d['slug']; ?>' href='http://www.officehour.fr/<?php echo $d['slug']; ?>' >
+            <?php if( empty($d['name']) ) { ?>
+                OfficeHour
+            <?php } else { ?>
+                <?php echo $d['name']; ?> sur OfficeHour
+            <?php } ?>
+        </a>
+        <script src='http://www.officehour.fr/widget/script.js/expert' type='text/javascript'></script>
+        <?php
+        }
+        
         echo $after_widget;
     }
 
@@ -33,15 +46,25 @@ class OfficeHour_Widget extends WP_Widget
     {
 	// default values
         $default = array(
-			'title' => 'test' 
+			'title' => 'OfficeHour',
+                        'slug' => '',
+                        'name' => ''
 		);
 
 	$d = wp_parse_args($d, $default);
 
         ?>
         <p>
-		<label for='<?php echo $this->get_field_id('title'); ?>'>Titre (facultatif) : </label>
+		<label for='<?php echo $this->get_field_id('title'); ?>'>Titre du Widget (facultatif)</label>
 		<input value='<?php echo $d['title']; ?>'  name='<?php echo $this->get_field_name('title'); ?>' id='<?php echo $this->get_field_id('title'); ?>' type='text'/>
+	</p>
+        <p>
+		<label for='<?php echo $this->get_field_id('name'); ?>'>Nom (facultatif)</label>
+                <input value='<?php echo $d['name']; ?>' placeholder='ex. Guillaume Warckol' name='<?php echo $this->get_field_name('name'); ?>' id='<?php echo $this->get_field_id('name'); ?>' type='text'/>
+	</p>
+        <p>
+            <label for='<?php echo $this->get_field_id('slug'); ?>'>Nom d'utilisateur OfficeHour (Aide : <a href='http://www.officehour.fr/expert/profile/description'>Mon Compte</a>)</label>
+                http://www.officehour.fr/<input value='<?php echo $d['slug']; ?>' placeholder='guillaume-warckol' name='<?php echo $this->get_field_name('slug'); ?>' id='<?php echo $this->get_field_id('slug'); ?>' type='text'/>
 	</p>
         <?php
     }
