f4bb8d69740993b15be627273ce34c17cfcd510e
root <root@62-210-114-154.online.net>
2015-01-05 17:38:46 +0100

    first files of our wordpress widget

--- officehour.php
+++ officehour.php
@@ -13,13 +13,44 @@ class OfficeHour_Widget extends WP_Widget
 {
     public function __construct()
     {
-        parent::__construct('officehour_widget', 'OfficeHour', array('description' => 'Widget OfficeHour'));
+        parent::__construct('officehour_widget', 'OfficeHour', array('description' => 'Widget OfficeHour','classname' => 'officehour-widget'));
     }
     
-    public function widget($args, $instance)
+    public function widget($args, $d)
     {
-        echo 'widget OfficeHour';
+        extract($args);
+
+        echo $before_widget;
+
+	if(isset($d['title']) && !empty($d['title'])) {
+        	echo $before_title.$d['title'].$after_title;
+	}
+
+        echo $after_widget;
+    }
+
+    public function form($d)
+    {
+	// default values
+        $default = array(
+			'title' => 'test' 
+		);
+
+	$d = wp_parse_args($d, $default);
+
+        ?>
+        <p>
+		<label for='<?php echo $this->get_field_id('title'); ?>'>Titre (facultatif) : </label>
+		<input value='<?php echo $d['title']; ?>'  name='<?php echo $this->get_field_name('title'); ?>' id='<?php echo $this->get_field_id('title'); ?>' type='text'/>
+	</p>
+        <?php
+    }
+
+    public function update($new, $old)
+    {
+         return $new;
     }
 }
+
 
 add_action('widgets_init', function(){register_widget('OfficeHour_Widget');});
